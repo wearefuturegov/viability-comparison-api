c51452dcb8446b38a773632cb410bfbe0f320ece
userman123 <dominic.freeman123@gmail.com>
2019-11-04 16:58:21 +0000

    Remove created_at and updated_at from response

--- app/serializers/viability_appraisal_serializer.rb
+++ app/serializers/viability_appraisal_serializer.rb
@@ -1,4 +1,4 @@
 class ViabilityAppraisalSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :local_authority, :local_authority_id, :application, :name, :latitude, :longitude, :date_submitted, :gross_development_value_pence, :construction_costs_pence, :professional_fees_pence, :marketing_and_letting_pence, :finance_pence, :financial_planning_obligations_pence, :developer_profit_pence, :residual_land_value_pence, :benchmark_land_value_pence, :residential_units, :habitable_rooms, :commercial_area_square_centimetres, :created_at, :updated_at
+  attributes :local_authority, :local_authority_id, :application, :name, :latitude, :longitude, :date_submitted, :gross_development_value_pence, :construction_costs_pence, :professional_fees_pence, :marketing_and_letting_pence, :finance_pence, :financial_planning_obligations_pence, :developer_profit_pence, :residual_land_value_pence, :benchmark_land_value_pence, :residential_units, :habitable_rooms, :commercial_area_square_centimetres
 end

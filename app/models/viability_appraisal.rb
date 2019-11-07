61466d8b7b4f202d799263bfa1d63b0b32edc68b
userman123 <dominic.freeman123@gmail.com>
2019-11-07 15:28:20 +0000

    Change > & < to >= & <= for habitable room filters

--- app/models/viability_appraisal.rb
+++ app/models/viability_appraisal.rb
@@ -1,5 +1,5 @@
 class ViabilityAppraisal < ApplicationRecord
-  scope :min_habitable_rooms, -> (min_habitable_rooms) { where("habitable_rooms > ?", min_habitable_rooms) }
-  scope :max_habitable_rooms, -> (max_habitable_rooms) { where("habitable_rooms < ?", max_habitable_rooms) }
+  scope :min_habitable_rooms, -> (min_habitable_rooms) { where("habitable_rooms >= ?", min_habitable_rooms) }
+  scope :max_habitable_rooms, -> (max_habitable_rooms) { where("habitable_rooms <= ?", max_habitable_rooms) }
   scope :directional_sort, -> (field) { order(field.start_with?('-') ? "#{field[1..-1]} desc NULLS LAST" : "#{field} asc NULLS LAST") }
 end
